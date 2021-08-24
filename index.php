e447adeb6f8609475932879b0d76cb66cd4fee23
domzalex <alexdomzalski@gmail.com>
2021-08-24 14:48:51 -0400

    landing page

--- index.php
+++ index.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Rate My Venue ATL</title>
 </head>
 <body>
-    <h1>RATEMYVENUE IS UNDER CONSTRUCTION</h1>
+    <nav>
+       
+    </nav>
+    <div class="main-container">
+        <h1>'Rate My Venue' is Under Construction</h1>
+        <p>If you would like to submit a venue review to help populate the website, please the following to <a href="mailto:admin@theculdesac.club">this email</a>: <ul>
+            <li>the venue name</li>
+            <li>a brief summary of your experience</li>
+            <li>and whether you were a performer or a guest</li>
+        </ul></p>
+        <!-- <form action="">
+            <input type="search" name="" id="" placeholder="Type in a venue name...">
+            <input type="submit" value="Submit"> 
+        </form>-->
+        <img src="img/johnny_automatic_piano.svg" alt="">
+    </div>
 </body>
 </html>
